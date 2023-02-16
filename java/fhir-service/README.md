10e9af2f394ad9dc39e5dd3d1795a448ac5d8df7
sameeragunarathne <sameera910409@gmail.com>
2023-02-17 03:06:30 +0530

    Add seperate FHIR service

---
+++ java/fhir-service/README.md
@@ -0,0 +1,10 @@
+# Choreo Samples
+
+## Sample FHIR Service
+
+#### Use the following build config when creating this component in Choreo:
+
+- Dockerfile: `java/fhir-service/Dockerfile`
+- Docker context: `java/fhir-service`
+- Port: `8080` 
+- OpenAPI filepath: `java/fhir-service/openapi.yaml`
---
+++ java/fhir-service/src/main/java/wso2/example/fhir/server/controller/ConformanceController.java
@@ -0,0 +1,38 @@
+package wso2.example.fhir.server.controller;
+
+import ca.uhn.fhir.context.FhirContext;
+import org.hl7.fhir.r4.model.CapabilityStatement;
+import org.hl7.fhir.r4.model.Enumerations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/fhir/metadata")
+public class ConformanceController {
+
+    @GetMapping
+    public String getConformanceDocument() {
+        FhirContext fhirContext = FhirContext.forR4();
+        CapabilityStatement capabilityStatement = new CapabilityStatement();
+        capabilityStatement.addFormat("json");
+        capabilityStatement.addFormat("xml");
+        capabilityStatement.setFhirVersion(Enumerations.FHIRVersion._0_4_0);
+        capabilityStatement.setKind(CapabilityStatement.CapabilityStatementKind.INSTANCE);
+        capabilityStatement.setName("Hapi FHIR Server");
+        capabilityStatement.setStatus(Enumerations.PublicationStatus.DRAFT);
+        capabilityStatement.setDate(new Date());
+        CapabilityStatement.CapabilityStatementRestComponent rest = new CapabilityStatement.CapabilityStatementRestComponent();
+        rest.setMode(CapabilityStatement.RestfulCapabilityMode.SERVER);
+        CapabilityStatement.ResourceInteractionComponent searchInteraction = new CapabilityStatement.ResourceInteractionComponent();
+        searchInteraction.setCode(CapabilityStatement.TypeRestfulInteraction.SEARCHTYPE);
+        CapabilityStatement.ResourceInteractionComponent readInteraction = new CapabilityStatement.ResourceInteractionComponent();
+        searchInteraction.setCode(CapabilityStatement.TypeRestfulInteraction.READ);
+        rest.addResource().setType("Patient").setProfile("http://hl7.org/fhir/StructureDefinition/Patient")
+                .addInteraction(searchInteraction).addInteraction(readInteraction);
+        capabilityStatement.addRest(rest);
+        return fhirContext.newJsonParser().encodeResourceToString(capabilityStatement);
+    }
+}
